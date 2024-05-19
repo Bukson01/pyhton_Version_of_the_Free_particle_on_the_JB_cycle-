cfe692254a484341c7cdd610853b418fb4b0e066
Umeh Chukwuebuka Emmanuel <81016410+Bukson01@users.noreply.github.com>
2024-05-20 00:48:12 +0100

    update

--- FP_on_JB.py
+++ FP_on_JB.py
@@ -5,3 +5,10 @@ from sympy import symbols, diff, solve
 
 # Define the range for the ratio of lengths (L3/L1)
 r = np.arange(-5, 5, 0.001)
+
+# Constants
+a = 3 * (4**(1/3) - 1)
+b = 1/4
+c = 1/4**(1/3)
+p = a * b
+q = a * c
